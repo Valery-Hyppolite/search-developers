7c695200481c0c883c82ba1341cdcf2ff1e89064
Valery <valeryhypp@gmail.com>
2022-05-19 22:56:01 -0400

    add env vars to database

--- devsearches/settings.py
+++ devsearches/settings.py
@@ -128,17 +128,17 @@ WSGI_APPLICATION = 'devsearches.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('AWSRDB_NAME'),
-#         'USER': os.environ.get('AWSRDB_USER'),
-#         'PASSWORD': os.environ.get('AWSRDB_PASWD'),
-#         'HOST': os.environ.get('AWSRDB_HOST'),
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('AWSRDB_NAME'),
+        'USER': os.environ.get('AWSRDB_USER'),
+        'PASSWORD': os.environ.get('AWSRDB_PASWD'),
+        'HOST': os.environ.get('AWSRDB_HOST'),
+        'PORT': '5432',
+    }
+}
 
 # DATABASES = {
 
@@ -153,17 +153,17 @@ WSGI_APPLICATION = 'devsearches.wsgi.application'
 # }
 
 #valou-admin databse account
-DATABASES = {
+# DATABASES = {
     
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'discover_devs',
-        'USER': 'valou',
-        'PASSWORD': 'Devvalou80k$',
-        'HOST': 'discover-dev-2022.c3ysewtjmgae.us-east-1.rds.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'discover_devs',
+#         'USER': 'valou',
+#         'PASSWORD': 'Devvalou80k$',
+#         'HOST': 'discover-dev-2022.c3ysewtjmgae.us-east-1.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 # DATABASES = {
 #
