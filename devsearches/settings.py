da5c1416ede80514223debbf78d8784f194b9947
Valery <valeryhypp@gmail.com>
2023-06-26 13:54:45 -0400

    rename env

--- devsearches/settings.py
+++ devsearches/settings.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 
 load_dotenv()
-env_path = Path(".")/'env.env'
+env_path = Path(".")/'.env'
 load_dotenv(dotenv_path=env_path)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
